e435e873a3278025c23f41f2fff7ff86ddec4dd0
Vijay Jadhav <vijayjadhav008@gmail.com>
2017-01-26 19:55:18 +0530

    newest

--- test.cpp
+++ test.cpp
@@ -1,33 +1,51 @@
-#include<bits/stdc++.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-#define ll long long int
-#define loop1(a,b) for(ll i=a;i<=b;i++)
-#define loop2(a,b) for(ll j=a;j<=b;j++)
-#define vt vector<ll>
-#define pb push_back
+#define MOD 1000000000L
+#define LL long long
+#define LIMIT 1001
 
-int main()
-{
-	int n;
-	cin>>n;
-	ll x=1;
-	vector<ll> req;
-	req.pb(1);
-	map<ll,ll> mm;
-	mm[1]=1;
-	while(n--){
-		ll a,b;
-		cin>>a>>b;
-		while(b--){
-			vector<ll> demo2;
-				for(vector<ll>::iterator i=req.begin();i!=req.end();i++){
-					if(mm[a*(*i)]==0){
-						demo2.pb(a*(*i));
-						mm[a*(*i)]=1;
-					}
-				}
-				for(vector<ll>::reverse_iterator i=demo2.rbegin();i!=demo2.rend();i++) req.pb(*i);
-		}
-	}
-	for(vector<ll>::iterator i=req.begin();i!=req.end();i++) out<<(*i)<<" ";
+vector<int> fact;
+
+void compute(){
+    fact.resize(LIMIT);
+    fact[0]=1;
+    for(int i=1;i<LIMIT;i++){
+        fact[i]=(i*fact[i-1])%MOD;
+    }
+}
+
+LL inverse(LL x){
+    LL p=MOD;
+    LL x0=0,x1=1,q,temp;
+    while(x>1){
+        q=x/p;
+        temp=p;
+        p=x%p;
+        x=temp;
+ 
+        temp=x0;
+        x0=x1-q*x0;
+        x1=temp;
+    }
+    if(x1<0) x1+=MOD;
+    return x1;
+}
+
+int main() {
+    compute();
+    int t;
+    cin>>t;
+    while(t--){
+        LL n;
+        cin>>n;
+        for(LL i=0;i<=n;i++){
+            cout<<((fact[n] * inverse(fact[i]))%MOD * inverse(fact[n-i]))%MOD<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
